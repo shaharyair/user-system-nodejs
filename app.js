f51ee0fd6e0550566fb223b14a837c1f4e6aa904
Shahar Yair <shaharyair12@gmail.com>
2023-06-06 23:44:42 +0300

    feat: added morgan middleware

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const morgan = require("morgan");
 const userRouter = require("./routes/userRouter");
 
 const app = express();
 app.use(express.json());
+
+app.use(morgan("tiny"));
 
 app.use("/users", userRouter);
 
