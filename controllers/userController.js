f8a2b8be4637bc9cc490942c0282fc03c295c062
Shahar Yair <shaharyair12@gmail.com>
2023-06-06 23:08:51 +0300

    added jwt token for login and register

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,11 @@
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
+
+const signToken = (id) => {
+  return jwt.sign({ userId: id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 // Register a new user
 const registerUser = async (req, res, next) => {
@@ -6,7 +13,11 @@ const registerUser = async (req, res, next) => {
     const { username, email, password } = req.body;
     const newUser = new User({ username, email, password });
     await newUser.save();
-    res.status(201).json({ message: "User registered successfully" });
+
+    // Generate JWT token
+    const token = signToken(newUser._id);
+
+    res.status(201).json({ message: "User registered successfully", token });
   } catch (error) {
     next(error);
   }
@@ -24,7 +35,11 @@ const loginUser = async (req, res, next) => {
     if (!isPasswordValid) {
       return res.status(401).json({ message: "Invalid email or password" });
     }
-    res.status(200).json({ message: "Login successful" });
+
+    // Generate JWT token
+    const token = signToken(user._id);
+
+    res.status(200).json({ message: "Login successful", token });
   } catch (error) {
     next(error);
   }
