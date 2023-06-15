bc26014e0f4a895f38d60471eb87d4e68678802e
Shahar Yair <shaharyair12@gmail.com>
2023-06-15 14:22:47 +0300

    feat: added global error controller middleware to app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const userRouter = require("./routes/userRouter");
+const globalErrorsController = require("./controllers/errorController");
 
 const app = express();
 app.use(express.json());
@@ -8,5 +9,7 @@ app.use(express.json());
 app.use(morgan("tiny"));
 
 app.use("/users", userRouter);
+
+app.use(globalErrorsController);
 
 module.exports = app;
