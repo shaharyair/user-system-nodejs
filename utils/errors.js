cace0f4eb05015311ec56fb195c8a656a5b4cdd3
Shahar Yair <shaharyair12@gmail.com>
2023-06-15 14:04:22 +0300

    feat(api)!: add error handlers

---
+++ utils/errors.js
@@ -0,0 +1,36 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.log(`Error: ${err}`);
+
+    res.status(500).json({
+      status: "error",
+      message: "An error occured",
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  if (process.env.NODE_ENV === "dev") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "prod") {
+    let error = { ...err };
+    sendErrorProd(error, res);
+  }
+};
