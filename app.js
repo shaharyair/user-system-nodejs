85aad0697c158cb1137311688668dea3200d2f76
Shahar Yair <shaharyair12@gmail.com>
2023-09-16 21:20:30 +0300

    auth: add request limiter for /api route
    limit users for a max of 100 requests in a hour for security

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
+const moment = require("moment");
+const rateLimit = require("express-rate-limit");
 const userRouter = require("./routes/userRouter");
 const globalErrorsController = require("./controllers/errorController");
 
@@ -7,6 +9,14 @@ const app = express();
 app.use(express.json());
 
 app.use(morgan("tiny"));
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: moment.duration(1, "hours").asMilliseconds(),
+  message: "Too many requests from this user, try again in an hour.",
+});
+
+app.use("/api", limiter);
 
 app.use("/api/users", userRouter);
 
